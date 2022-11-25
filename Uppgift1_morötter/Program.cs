1ef32b88e2901d706eaec800d953bc111ccc6bf3
Admin <Admin@TEB127007>
2022-11-25 10:04:55 +0100

    Add project files.

---
+++ Uppgift1_morötter/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Uppgift1_morötter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur lång tid tar det för Tor att äta en morot? Svara i sekunder, mellan 1-100.");
+            int torsTid = int.Parse(Console.ReadLine());
+            Console.WriteLine("Hur lång tid tar det för mor att äta en morot? Svara i sekunder, mellan 1-100.");
+            int morsTid = int.Parse(Console.ReadLine());
+
+
+        }
+    }
+}
